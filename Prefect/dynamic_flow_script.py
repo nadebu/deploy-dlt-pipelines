95b8b561c877d787c8309206223f7cb30fe6ac36
Joel Mulimba <nadebu@gmail.com>
2025-11-25 15:09:01 +0000

    inital commit

---
+++ Prefect/dynamic_flow_script.py
@@ -0,0 +1,30 @@
+import dlt
+
+from prefect import flow, task
+
+
+@task(log_prints=True)
+def run_resource(resource_name: str):
+    import github_pipeline
+    source = github_pipeline.github_source.with_resources(resource_name)
+    pipeline = dlt.pipeline(
+        pipeline_name=f"github_dynamic_{resource_name}",
+        destination="bigquery",
+        dataset_name="demo__dynamic_github",
+        progress="log"
+    )
+
+    info = pipeline.run(source)
+    print(f"{resource_name} -> {info}")
+    return info
+
+@flow(log_prints=True)
+def main():
+
+    a = run_resource("repos")
+    b = run_resource("contributors")
+    c = run_resource("releases")
+    return a, b, c
+
+if __name__ == "__main__":
+    main.serve(name="dynamic-deployment")
